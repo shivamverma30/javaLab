e8190918e1a47effd6beacf3ab3d4502c63b9496
shivam verma <24204031116@stu.manit.ac.in>
2025-08-01 15:06:12 +0530

    added a6

---
+++ a6.java
@@ -0,0 +1,23 @@
+//Implement a program to sort an array of integers in ascending order using a loop.
+
+public class a6 {
+    public static void main(String[] args) {
+        int a[] = {34, 45, 45, 22, 32, 34, 45, 65};
+        int n = a.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    // Swap a[j] and a[j + 1]
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+        // Printing sorted array using normal for loop
+        System.out.println("Sorted array in ascending order:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
